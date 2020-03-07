36d7f2888dd125853e1cef36b3d8cec5dad967e1
shawheenattar <shawheen14@gmail.com>
2020-03-06 23:14:54 -0600

    added meals to mealplanner, added pics to healthy subs, updated about page with dynamic info, added new css file

--- js/main.js
+++ js/main.js
@@ -1,48 +1,4 @@
-//pie chart
-var ctxP = document.getElementById("pieChart").getContext('2d');
-var myPieChart = new Chart(ctxP, {
-    plugins: [ChartDataLabels],
-    type: 'pie',
-    data: {
-        labels: ["Carbohydrates", "Protein", "Fat"],
-        datasets: [{
-            data: [50, 30, 20],
-            backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C"],
-            hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870"]
-        }]
-    },
-    options: {
-        responsive: true,
-        legend: {
-          position: 'right',
-          labels: {
-            padding: 20,
-            boxWidth: 10
-          }
-        },
-        plugins: {
-          datalabels: {
-            formatter: (value, ctx) => {
-              let sum = 0;
-              let dataArr = ctx.chart.data.datasets[0].data;
-              dataArr.map(data => {
-                sum += data;
-              });
-              let percentage = (value * 100 / sum).toFixed(0) + "%";
-              return percentage;
-            },
-            color: 'white',
-            labels: {
-              title: {
-                font: {
-                  size: '16'
-                }
-              }
-            }
-          }
-        }
-    }
-});
+
 
 jQuery.fn.loadRepositories = function () {
     var target = this;
@@ -87,34 +43,6 @@ jQuery.fn.loadRepositories = function () {
                     console.log(totalCommits);
                     console.log(dict);
                 });
-
-                $.getJSON(url + "/issues", {}, function (data) {
-                    console.log(data);
-                    var totalIssues = data.length;
-                    console.log(totalIssues);
-                    var dict = {
-                        'shawheenattar': 0,
-                        'adrianmb0': 0,
-                        'ReedHopkins': 0,
-                        'cameronsanders': 0,
-                        'ramyarajasekaran': 0,
-                        'evanchang2399': 0
-                    };
-
-                    $(data).each(function () {
-                        dict[this.user.login] = dict[this.user.login] + 1;
-                    })
-
-                    list.append('<p>Total # of issues: ' + totalIssues + '</p>');
-
-                    for (var key in dict) {
-                        var val = dict[key];
-                        list.append('<p>' + key + ': ' + val + ' issues</p>');
-                    }
-
-                    console.log(totalIssues);
-                    console.log(dict);
-                });
             }
         });
     });
@@ -146,7 +74,7 @@ jQuery.fn.loadContributorStats = function (username) {
                         if (this.author.login == username) commits = commits + 1;
                     })
 
-                    list.append('<p>Number of commits: ' + commits + '</p>');
+                    list.append('<p><strong>Number of commits: </strong>' + commits + '</p>');
                 });
 
                 $.getJSON(url + "/issues", {}, function (data) {
@@ -155,7 +83,7 @@ jQuery.fn.loadContributorStats = function (username) {
                         if (this.user.login == username) issues = issues + 1;
                     })
 
-                    list.append('<p>Number of issues: ' + issues + '</p>');
+                    list.append('<p><strong>Number of issues: </strong>' + issues + '</p>');
                 });
             }
         });
