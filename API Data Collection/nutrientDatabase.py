2721fd1661d48706ac3eff75eb6b506b428eeedc
Evan Chang <echang3493@utexas.edu>
2020-03-22 16:11:52 -0400

    added more nutrient info to the query and started setting up MongoDB

--- API Data Collection/nutrientDatabase.py
+++ API Data Collection/nutrientDatabase.py
@@ -1,11 +1,14 @@
 from pprint import pprint
 from nutritionix import Nutritionix
+from pymongo import MongoClient
 import requests
 import json
 import string
 
+client = MongoClient("mongodb://projectUser:team7@:27017")
+db = client.business
 #get all relevant information for items coming up in the query of "pie"
-url = "https://api.nutritionix.com/v1_1/search/pie?fields=item_name&appId=973d994f&appKey=e5b17ecd535365487edd736593279f89"
+url = "https://api.nutritionix.com/v1_1/search/pie?fields=nf_total_fat,nf_sugars,nf_sodium,nf_cholesterol,nf_protein&appId=973d994f&appKey=e5b17ecd535365487edd736593279f89"
 headerInfo = {"x-app-id": "973d994f", "x-app-key": "e5b17ecd535365487edd736593279f89"}
 parameters = {"fields":"item_name"}
 response = requests.get(url = url, headers = headerInfo)
