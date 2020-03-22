973de8bb8762ea530021cb5944bef25cd0560243
Evan Chang <echang3493@utexas.edu>
2020-03-22 17:17:18 -0400

    added comments and made code much easier for everyone to read

--- API Data Collection/nutrientDatabase.py
+++ API Data Collection/nutrientDatabase.py
@@ -5,18 +5,23 @@ import requests
 import json
 import string
 
+#Set up client and database
 client = MongoClient('mongodb://localhost:27017/')
 #client = MongoClient("mongodb://projectUser:team7@:27017")
 db = client.wellbeing
+
 #get all relevant information for items coming up in the query of "pie"
 url = "https://api.nutritionix.com/v1_1/search/pie?fields=item_name,nf_total_fat,nf_sugars,nf_sodium,nf_cholesterol,nf_protein&appId=973d994f&appKey=e5b17ecd535365487edd736593279f89"
 headerInfo = {"x-app-id": "973d994f", "x-app-key": "e5b17ecd535365487edd736593279f89"}
 parameters = {"fields":"item_name"}
+
+#jsonData holds the response as a JSON array
+#asString holds the JSON as a string
+#items is the actual information with all the items that the query returned
 response = requests.get(url = url, headers = headerInfo)
 jsonData = response.json()
 asString = response.text
-dataDict = json.loads(asString)
-items = dataDict["hits"]
+items = jsonData["hits"]
 collection = db.nutrients
 
 
@@ -25,6 +30,6 @@ for item in items:
     print(item["fields"])
     print("\n")
     collection.insert_one(item["fields"])
-    #db.nutrients.insert_one(item["fields"])
 
+#retrieve one instance in collection 
 print(db.nutrients.find_one())
