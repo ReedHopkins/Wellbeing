9883c2173378e12e8c7da1f81c70fbb92eb75bfc
Evan Chang <echang3493@utexas.edu>
2020-03-22 17:13:16 -0400

    successfully added some items to database

--- API Data Collection/nutrientDatabase.py
+++ API Data Collection/nutrientDatabase.py
@@ -5,17 +5,26 @@ import requests
 import json
 import string
 
-client = MongoClient("mongodb://projectUser:team7@:27017")
-db = client.business
+client = MongoClient('mongodb://localhost:27017/')
+#client = MongoClient("mongodb://projectUser:team7@:27017")
+db = client.wellbeing
 #get all relevant information for items coming up in the query of "pie"
-url = "https://api.nutritionix.com/v1_1/search/pie?fields=nf_total_fat,nf_sugars,nf_sodium,nf_cholesterol,nf_protein&appId=973d994f&appKey=e5b17ecd535365487edd736593279f89"
+url = "https://api.nutritionix.com/v1_1/search/pie?fields=item_name,nf_total_fat,nf_sugars,nf_sodium,nf_cholesterol,nf_protein&appId=973d994f&appKey=e5b17ecd535365487edd736593279f89"
 headerInfo = {"x-app-id": "973d994f", "x-app-key": "e5b17ecd535365487edd736593279f89"}
 parameters = {"fields":"item_name"}
 response = requests.get(url = url, headers = headerInfo)
 jsonData = response.json()
 asString = response.text
 dataDict = json.loads(asString)
+items = dataDict["hits"]
+collection = db.nutrients
 
-for value in dataDict:
-    if value == "hits":
-        print(dataDict["hits"])
+
+for item in items:
+    
+    print(item["fields"])
+    print("\n")
+    collection.insert_one(item["fields"])
+    #db.nutrients.insert_one(item["fields"])
+
+print(db.nutrients.find_one())
