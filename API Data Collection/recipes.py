4a3d31e1c2e35f535fc3d97ad4ab6d0e68010767
ramyarajasekaran <ramyaraj@utexas.edu>
2020-03-23 13:56:21 -0500

    setup MongoDB, inserted few recipes

--- API Data Collection/recipes.py
+++ API Data Collection/recipes.py
@@ -1,7 +1,14 @@
 import requests
+from pymongo import MongoClient
+import json
+from bson import json_util
+
+#set up client
+client = MongoClient('mongodb://localhost:27017/')
+#client = MongoClient("mongodb://projectUser:team7@:27017")
+db = client.wellbeing
 
 url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate"
-
 querystring = {"targetCalories":"2000","timeFrame":"day"}
 
 headers = {
@@ -10,5 +17,19 @@ headers = {
     }
 
 response = requests.request("GET", url, headers=headers, params=querystring)
+jsonData = response.json()
+asString = response.text
+items = jsonData["meals"]
+collection = db.meals
 
-print(response.text)
+for item in items:
+
+	print(item)
+	print("\n")
+	collection.insert_one(item)
+
+#retrieve one instance in collection 
+json_formatted_str = json.dumps(db.meals.find_one(), indent=2,default=json_util.default)
+print(json_formatted_str)
+
+
