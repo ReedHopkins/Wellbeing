76ac119825fd764dbca2c73ba03a28d21d53b9ac
ramyarajasekaran <ramyaraj@utexas.edu>
2020-03-29 11:13:36 -0500

    added unit tests for recipe collection

--- API Data Collection/recipesTest.py
+++ API Data Collection/recipesTest.py
@@ -1,3 +1,4 @@
+import unittest
 from pytest_mongodb.plugin import mongo_engine
 from pytest import mark
 import pymongo
@@ -17,25 +18,26 @@ DOCUMENT = {
     "gaps":"no","lowFodmap":False,
     }
 
-def test_recipe_insert(mongodb):
-    assert mongodb.recipe.insert_one(DOCUMENT)
-    
-def test_recipe_remove(mongodb):
-    assert mongodb.recipe.delete_one(DOCUMENT)
-    
-def test_recipe_is_there(mongodb):
-    assert mongodb.recipe.find_one(DOCUMENT)
+class TestRecipe(unittest.TestCase):
 
-@mark.skipif(mongo_engine() == 'mongomock', reason="mongomock does not support that")
-def test_recipe(mongodb):
-    assert 'recipe' in mongodb.list_collection_names()
-    food = mongodb.recipe.find_one(DOCUMENT)
-    assert food['cheap'] == False
-    
-if __name__ == "__main__":
+    def test_recipe_insert(mongodb):
+        assert tbl.insert_one(DOCUMENT)
+
+    def test_recipe_remove(mongodb):
+        assert tbl.delete_one(DOCUMENT)
+
+    def test_recipe_is_there(mongodb):
+        assert tbl.find_one(DOCUMENT)
+
+    def test_recipe(mongodb):
+        assert 'recipeData' in collections
+        food = tbl.find_one(DOCUMENT)
+        assert food['cheap'] == False
+
+if __name__ == '__main__':
     client = pymongo.MongoClient("mongodb://projectUser:team7@cluster0-shard-00-00-rwcw3.mongodb.net:27017,cluster0-shard-00-01-rwcw3.mongodb.net:27017,cluster0-shard-00-02-rwcw3.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
-    mydb = client["wellbeing"]
-    test_recipe(mydb)
-    test_recipe_insert(mydb)
-    test_recipe_is_there(mydb)
-    test_recipe_remove(mydb)
+    mongodb = client["wellbeing"]
+    tbl = mongodb.recipes
+    collections = mongodb.list_collection_names()
+    unittest.main()
+
