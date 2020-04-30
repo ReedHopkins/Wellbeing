939ba41ae6b138bff8183e8ee3a1f69fd4df3b0e
shawheenattar <shawheen14@gmail.com>
2020-04-30 17:12:43 -0500

    refactored ModelServlet.java

--- MongoDbJspServlet/src/main/java/com/jcg/mongodb/servlet/ModelServlet.java
+++ MongoDbJspServlet/src/main/java/com/jcg/mongodb/servlet/ModelServlet.java
@@ -17,88 +17,41 @@ public class ModelServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
-		String model = request.getParameter("model");
-		String search_term = request.getParameter("search_term");
-		String sort = request.getParameter("sort");
-		
-		boolean nosearch = search_term == null || "".equals(search_term);
-		boolean nosort = sort == null || "".equals(sort);
-		
-		if (search_term == null) search_term = "";
-		if (sort == null) sort = "";
-
-		// Retrieve all elements in database based on model
-		DatabaseSingleton.getInstance();
-		ArrayList<?> list = new ArrayList<Object>();
-		
-		if (!nosort) {
-			if (model.equals("Ingredient")) list = DatabaseUtility.getSortedIngredients(sort);
-			if (model.equals("Recipe")) list = DatabaseUtility.getSortedRecipes(sort);
-			if (model.equals("Nutrient")) list = DatabaseUtility.getSortedNutrients(sort);
-		} else {
-			if (model.equals("Ingredient")) list = DatabaseSingleton.getInstance().getIngredients();
-			if (model.equals("Recipe")) list = DatabaseSingleton.getInstance().getRecipes();
-			if (model.equals("Nutrient")) list = DatabaseSingleton.getInstance().getNutrients();
-		}
-		
-		int size = list.size();
-
-		// begin pagination calculations
-		String spageid = request.getParameter("page");
-		if (spageid == null) {
-			spageid = "1";
-		}
-		String subtitle = "All " + model + "s (Page " + spageid + "):";
-		
-		if (nosearch) {
-			
-			Paginator Paginator = new Paginator(model, spageid, size, 9);
-			if (!nosort) Paginator.setSortTerm(sort);
-			ArrayList<?> subList = new ArrayList<Object>(list.subList(Paginator.getStartIndex(), Paginator.getEndIndex()));
-			String params = "&sort=" + sort;
-
-			// set calculated attributes
-			request.setAttribute("subtitle", subtitle);
-			request.setAttribute(model.toLowerCase(), subList);
-			request.setAttribute("pageNums", Paginator.getPageNums());
-			request.setAttribute("previous", Paginator.getPreviousPageLink());
-			request.setAttribute("next", Paginator.getNextPageLink());
-			request.setAttribute("first", Paginator.getFirstPageLink());
-			request.setAttribute("last", Paginator.getLastPageLink());
-			request.setAttribute("params", params);
-			request.getRequestDispatcher(model.toLowerCase() + "s.jsp").forward(request, response);
-
-		} else {
-			doPost(request, response);
-		}
-
+		loadPage(request, response);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
+		loadPage(request, response);
+	}
+	
+	protected void loadPage(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
 		String model = request.getParameter("model");
 		String search_term = request.getParameter("search_term");
-		
+		String sort = request.getParameter("sort");
 		String spageid = request.getParameter("page");
 		if (spageid == null) {
 			spageid = "1";
 		}
 		
-		search_term = search_term.toLowerCase();
+		boolean nosearch = search_term == null || "".equals(search_term);
+		boolean nosort = sort == null || "".equals(sort);
 		
-		String subtitle = "Search Results (Page " + spageid + "):";
+		if (search_term == null) search_term = "";
+		if (sort == null) sort = "";
+		search_term = search_term.toLowerCase();
 
-		// Search for matched nutrients
-		DatabaseSingleton.getInstance();
-		ArrayList<?> list = new ArrayList<Object>();		
-			
-		if (model.equals("Ingredient")) list = DatabaseUtility.searchIngredients(search_term);
-		if (model.equals("Recipe")) list = DatabaseUtility.searchRecipes(search_term);
-		if (model.equals("Nutrient")) list = DatabaseUtility.searchNutrients(search_term);
+		// Retrieve all elements in database based on model
+		ArrayList<?> list = getList(model, nosearch, nosort, search_term, sort);
 		
 		int size = list.size();
+		String subtitle = "";
+		if (nosearch) subtitle = "All " + model + "s (Page " + spageid + "):";
+		else subtitle = "Search Results (Page " + spageid + "):";
 
 		Paginator Paginator = new Paginator(model, spageid, size, 9);
 		Paginator.setSearchTerm(search_term);
@@ -111,6 +64,8 @@ public class ModelServlet extends HttpServlet {
 			subtitle = "No results found...";
 		}
 		String params = "&search_term=" + search_term;
+		String show_param = "none";
+		if (search_term.length() > 0) show_param = "block";
 
 		// set calculated attributes
 		request.setAttribute("subtitle", subtitle);
@@ -123,9 +78,30 @@ public class ModelServlet extends HttpServlet {
 		request.setAttribute("params", params);
 		
 		request.setAttribute("search_term", search_term);
-		request.setAttribute("show_param", "block");
+		request.setAttribute("show_param", show_param);
 		request.setAttribute("showPagination", showPagination);
 		request.getRequestDispatcher(model.toLowerCase() + "s.jsp").forward(request, response);
+		
+	}
+	
+	private ArrayList<?> getList(String model, boolean nosearch, boolean nosort, String search_term, String sort) {
+		DatabaseSingleton.getInstance();
+		ArrayList<?> list = new ArrayList<Object>();
+		
+		if (nosort && nosearch) {
+			if (model.equals("Ingredient")) list = DatabaseSingleton.getInstance().getIngredients();
+			if (model.equals("Recipe")) list = DatabaseSingleton.getInstance().getRecipes();
+			if (model.equals("Nutrient")) list = DatabaseSingleton.getInstance().getNutrients();
+		} else if (nosearch && !nosort) {
+			if (model.equals("Ingredient")) list = DatabaseUtility.getSortedIngredients(sort);
+			if (model.equals("Recipe")) list = DatabaseUtility.getSortedRecipes(sort);
+			if (model.equals("Nutrient")) list = DatabaseUtility.getSortedNutrients(sort);
+		} else {
+			if (model.equals("Ingredient")) list = DatabaseUtility.searchIngredients(search_term);
+			if (model.equals("Recipe")) list = DatabaseUtility.searchRecipes(search_term);
+			if (model.equals("Nutrient")) list = DatabaseUtility.searchNutrients(search_term);
+		}
+		return list;
 	}
 
 }
